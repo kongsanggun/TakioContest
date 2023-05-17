61bf5a981989417f41f3a33b2258aee22fb94e4b
kongsanghyean <dodoi77@naver.com>
2023-05-17 20:13:41 +0900

    [Front-End] 랭킹 API 한번만 불러오게 수정

--- client/components/bar/barDataChogosu.js
+++ client/components/bar/barDataChogosu.js
@@ -4,49 +4,12 @@ import React, { useRef, useEffect, useState } from "react";
 import Chart from "@toast-ui/chart";
 import "@toast-ui/chart/dist/toastui-chart.min.css"; // Chart 스타일
 
-export default function BarData({date}) {
-  const [size, setSize] = useState(0);
-  const [labels] = useState([]);
-  const [songScore1] = useState([]);
-  const [songScore2] = useState([]);
-  const [songScore3] = useState([]);
-
+export default function BarData({date, size, labels, songScore1, songScore2, songScore3}) {
   useEffect(() => {
     initBar();
   }, []);
 
   async function initBar() {
-    try {
-      const setBar = await fetch(`/ranking/chogosu`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(message);
-          }
-          return res.json();
-        })
-        .then(async (data) => {
-          const rankData = data.data.contents;
-          setSize(rankData.length); // 참가자 수
-
-          rankData.forEach((element, index) => {
-            labels.push(element.entryName);
-
-            songScore1.push(element.songScore1);
-            songScore2.push(element.songScore2);
-            songScore3.push(element.songScore3);
-          });
-        });
-    } catch (Error) {
-      Router.push("/error");
-      return;
-    }
-
     const el = document.getElementById("chart");
     const data = {
       categories: labels,
--- client/pages/ranking.tsx
+++ client/pages/ranking.tsx
@@ -13,10 +13,22 @@ export default function Entry() {
         end: "",
     });
 
+    const [sizeO, setSizeO] = useState(0);
+    const [labelO] = useState([]);
+    const [songScore1O] = useState([]);
+    const [songScore2O] = useState([]);
+    const [songScore3O] = useState([]);
+
+    const [sizeC, setSizeC] = useState(0);
+    const [labelC] = useState([]);
+    const [songScore1C] = useState([]);
+    const [songScore2C] = useState([]);
+    const [songScore3C] = useState([]);
+
     useEffect(() => {
         init();
-        // TODO : 마감 시간 임박 시 정보 가리기
-    },[]);
+        initBar();
+    }, []);
 
     const init = async () => {
         try {
@@ -32,16 +44,76 @@ export default function Entry() {
                 }
                 return res.json();
             }).then(async (data) => {
-                    const start = data.start.split('T')[0];
-                    const end = data.end.split('T')[0];
+                const start = data.start.split('T')[0];
+                const end = data.end.split('T')[0];
 
-                    setDates({
-                        start: start,
-                        end: end
-                    });
-
+                setDates({
+                    start: start,
+                    end: end
                 });
 
+            });
+            // TODO : 마감 시간 임박 시 정보 가리기
+
+        } catch (Error) {
+            Router.push("/error");
+            return;
+        }
+    }
+
+    const initBar = async () => {
+        try {
+            const setBarO = await fetch(`/ranking/origin`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                },
+            })
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error();
+                    }
+                    return res.json();
+                })
+                .then(async (data) => {
+                    const rankData = data.data.contents;
+                    setSizeO(rankData.length); // 참가자 수
+
+                    rankData.forEach((element: { entryName: never; songScore1: never; songScore2: never; songScore3: never; }, index: any) => {
+                        labelO.push(element.entryName);
+
+                        songScore1O.push(element.songScore1);
+                        songScore2O.push(element.songScore2);
+                        songScore3O.push(element.songScore3);
+                    });
+                });
+
+            const setBarC = await fetch(`/ranking/chogosu`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                },
+            })
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error();
+                    }
+                    return res.json();
+                })
+                .then(async (data) => {
+                    const rankData = data.data.contents;
+                    setSizeC(rankData.length); // 참가자 수
+
+                    rankData.forEach((element: { entryName: never; songScore1: never; songScore2: never; songScore3: never; }, index: any) => {
+                        labelC.push(element.entryName);
+
+                        songScore1C.push(element.songScore1);
+                        songScore2C.push(element.songScore2);
+                        songScore3C.push(element.songScore3);
+                    });
+                });
         } catch (Error) {
             Router.push("/error");
             return;
@@ -52,7 +124,7 @@ export default function Entry() {
         mode ? setMode(false) : setMode(true);
     }
 
-    const OriginBar= dynamic(
+    const OriginBar = dynamic(
         () => import('../components/bar/barDataOrigin'),
         { ssr: false }
     )
@@ -78,16 +150,16 @@ export default function Entry() {
                 <div className='w-auto h-10 text-[#121316] flex items-center justify-start font-["SDKukdetopokki-Lt"] font-extrabold duration-200'>
                     {
                         mode ?
-                        <button className = "w-auto h-full text-xl font-light mr-7 flex items-center justify-center duration-150 text-[#BEC0D7] border-b-2" disabled>오리지널</button>
-                        :
-                        <button className = "w-auto h-full text-xl font-light mr-7 flex items-center justify-center duration-150 text-[#121316] hover:text-[#BEC0D7] hover:border-b-2" onClick={changeMode}>오리지널</button>
+                            <button className="w-auto h-full text-xl font-light mr-7 flex items-center justify-center duration-150 text-[#BEC0D7] border-b-2" disabled>오리지널</button>
+                            :
+                            <button className="w-auto h-full text-xl font-light mr-7 flex items-center justify-center duration-150 text-[#121316] hover:text-[#BEC0D7] hover:border-b-2" onClick={changeMode}>오리지널</button>
                     }
                     {
                         mode ?
-                        <button className = "w-auto h-full text-xl font-light mx-7 flex items-center justify-center duration-150 text-[#121316] hover:text-[#BEC0D7] hover:border-b-2" onClick={changeMode}>초고수</button>
-                        :
-                        <button className = "w-auto h-full text-xl font-light mx-7 flex items-center justify-center duration-150 text-[#BEC0D7] border-b-2" disabled>초고수</button>
-                    }  
+                            <button className="w-auto h-full text-xl font-light mx-7 flex items-center justify-center duration-150 text-[#121316] hover:text-[#BEC0D7] hover:border-b-2" onClick={changeMode}>초고수</button>
+                            :
+                            <button className="w-auto h-full text-xl font-light mx-7 flex items-center justify-center duration-150 text-[#BEC0D7] border-b-2" disabled>초고수</button>
+                    }
                 </div>
             </div>
         )
@@ -103,8 +175,10 @@ export default function Entry() {
                         <div>Do the G 2023 대회의 랭킹입니다.</div>
                     </div>
                     <SubManu />
-                    {mode ? <OriginBar date = {dates}/> : <ChogosuBar date = {dates}/>}
-                    {mode ? <OriginGird/> : <ChogosuGird/>}
+                    {mode ? <OriginBar date={dates} size={sizeO} labels={labelO} songScore1={songScore1O} songScore2={songScore2O} songScore3={songScore3O} />
+                        :
+                        <ChogosuBar date={dates} size={sizeC} labels={labelC} songScore1={songScore1C} songScore2={songScore2C} songScore3={songScore3C} />}
+                    {mode ? <OriginGird /> : <ChogosuGird />}
                 </div>
             </div>
             <Footer />
